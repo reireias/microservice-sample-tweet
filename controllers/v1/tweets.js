b29e33dfc2d564e8a15285101cb79853cd2cca37
reireias <reireias@gmail.com>
2019-03-10 09:27:37 +0900

    add controllers

--- controllers/v1/tweets.js
+++ controllers/v1/tweets.js
@@ -5,4 +5,16 @@ router.get('/', (req, res, next) => {
   res.status(200).json({ message: 'hello' })
 })
 
+router.get('/:id', (req, res, next) => {
+  res.status(200).json({ message: 'hello' })
+})
+
+router.post('/', (req, res, next) => {
+  res.status(200).json({ message: 'hello' })
+})
+
+router.delete('/:id', (req, res, next) => {
+  res.status(200).json({ message: 'hello' })
+})
+
 module.exports = router
